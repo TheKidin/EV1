6bb954c4c58c262f1009ac34695782d7c473f03c
TheKidin <94573812+TheKidin@users.noreply.github.com>
2023-11-16 17:32:41 -0600

    Delete lectoralmacen.py

--- lectoralmacen.py
+++
@@ -1,7 +0,0 @@
-archi1 = open("datos.txt", "r")
-lineas = archi1.readlines()
-print('El archivo tiene', len(lineas), 'lineas')
-print('El contenido del archivo')
-for linea in lineas:
-    print(lineas, end='')
-archi1.close()
