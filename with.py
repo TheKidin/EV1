bea6a804dc9d2b52e1ef1313f5f7e0406d60c388
TheKidin <94573812+TheKidin@users.noreply.github.com>
2023-11-16 17:31:08 -0600

    Delete with.py

--- with.py
+++
@@ -1,8 +0,0 @@
-with open("colores.txt", "r") as f:
-    contenido = f.read()
-    print(contenido)
-
-# comporbamos que,aun que no se aplico el colse(), el archivo
-# esta cerrado
-
-print("¿Archivo cerrado?", f.closed)
