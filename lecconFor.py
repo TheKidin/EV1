e82e77b30646b639f06469188bd82d85ee00e742
TheKidin <94573812+TheKidin@users.noreply.github.com>
2023-11-16 17:33:57 -0600

    Delete lecconFor.py

--- lecconFor.py
+++
@@ -1,8 +0,0 @@
-# abro el archivo modo lectura
-archivo = "colores.txt"
-f = open(archivo, "r")
-# leo unicamente los primeros 5 caracteres del archivo
-for linea in f:
-    print(">", linea)
-# cierre el archivo
-f.close()
